22c585c235ed6aabf1ef26046d6e5802bdd07d58
GZOZO94 <gaszol11.c@gmail.com>
2017-11-04 21:07:23 +0100

    5

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
   res.tpl = {};
   res.tpl.error = [];
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   return next();
 })
 /**
