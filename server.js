4834ad5eb2e815849ac733edee12e23b8d191910
GZOZO94 <gaszol11.c@gmail.com>
2017-11-04 17:47:01 +0100

    3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ejs=require('ejs');
 const pg = require('pg');
 const connectionString="postgres://czdwudvylkbbrd:b07dba3737e5a2324ae060004d7afd41e54bd2fc0561bc068c8ef42921d97c8b@ec2-46-51-187-253.eu-west-1.compute.amazonaws.com:5432/d34ai1jentrfoc";
 pg.defaults.ssl = true;
-
+const port = process.env.PORT || 3000;
 app.use(express.static('static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -33,6 +33,6 @@ app.use(function(err,req,res,next){
 	res.status(500).send('OMG What happend?!');
 	console.error(err.stack);
 });
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 	console.log("Server is running on port: 3000")
 });
